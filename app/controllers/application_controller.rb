0399e955daf5f4898e46a6be9d38a6358e756bc5
Voldemar Duletskiy <voldemar.duletskiy@gmail.com>
2018-08-07 19:21:53 +0200

    asdasd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+#
+# class name
+#
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :do_something
+
+  def do_something
+    puts 123
+  end
 end
